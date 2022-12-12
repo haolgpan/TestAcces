f6e16d2cbd877537a5a4344e0f7f3807044959d7
haolgpan <haolgpan@gmail.com>
2022-12-12 01:35:37 +0100

    DOING MY BEST 5.0

--- src/main/java/Spices.java
+++ src/main/java/Spices.java
@@ -4,9 +4,17 @@ import jakarta.xml.bind.annotation.XmlRootElement;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * Classe que crea un arraylist para guardar la lista de especias que guardaremos para luego manejarlo en el código
+ */
 @XmlRootElement(name = "SPICES")
 public class Spices {
     List<Spice> spices;
+
+    /**
+     * Método que permite acceder a la arraylist spices
+     * @return Devuelve un Arraylist de especias
+     */
     public List<Spice> getSpices(){
         return spices;
     }
@@ -14,6 +22,11 @@ public class Spices {
     public void setSpices(List<Spice> spices){
         this.spices = spices;
     }
+
+    /**
+     * Este método recibe por parámetro un objeto spice y lo añade a la arraylist spices
+     * @param spice
+     */
     public void add(Spice spice){
         if(this.spices == null){
             this.spices = new ArrayList<Spice>();
